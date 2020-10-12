0dd23c0adf32265e8a608c3c254130799ec7b465
Muhesh7 <vmspride7@gmail.com>
2020-10-12 14:19:33 +0530

    feat(stickerapp):insert boilerplate code

---
+++ app/src/main/java/com/example/stickermaker/CropFragment.java
@@ -0,0 +1,23 @@
+package com.example.stickermaker;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CropFragment extends Fragment {
+
+    public CropFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_crop, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/stickermaker/EditorFragment.java
@@ -0,0 +1,24 @@
+package com.example.stickermaker;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EditorFragment extends Fragment {
+
+
+    public EditorFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_editor, container, false);
+    }
+}
